b7f7ed45fc81a7cd26121e239b4b041cadc8a316
Yoann Abran <y.abran@codeur.online>
2020-07-08 16:59:31 +0200

    test

--- index.php
+++ index.php
@@ -25,34 +25,36 @@
     //   echo $row['first_name'] .'&nbsp'. $row['last_name'] . "<br>";
     // }
 
-    //les palmer
+    //1.les palmer
 //     $sql = "SELECT last_name, first_name FROM `table 1` WHERE last_name = 'palmer'";
 //     foreach ($conn -> query($sql) as $row) {
 // echo $row['last_name'] .'&nbsp'. $row['first_name'] . "<br>";
 
-//les femmes
+//2.les femmes
 // $sql = "SELECT last_name, first_name, gender FROM `table 1` WHERE gender = 'female'";
 // foreach ($conn -> query($sql) as $row) {
 // echo $row['last_name'] .'&nbsp'. $row['first_name'] .'&nbsp'. $row['gender']. "<br>";
 
-//etats commence par n
+//3.etats commence par n
 // $sql = "SELECT country_code FROM `table 1` WHERE country_code LIKE 'n%'";
 // foreach ($conn -> query($sql) as $row) {
 // echo $row['country_code']. "<br>";
 // }
 
-//email contenant google
+//4.email contenant google
 // $sql = "SELECT last_name, first_name, email FROM `table 1` WHERE email LIKE '%google%'";
 // foreach ($conn -> query($sql) as $row) {
 // echo $row['last_name'] .'&nbsp'. $row['first_name'] .'&nbsp'. $row['email']. "<br>";
 
-//répartition par etat +nombre d'enregistrement par état croissant
-$sql = "SELECT country_code, COUNT(*) as count FROM `table 1` GROUP BY country_code ORDER BY count";
-foreach ($conn -> query($sql) as $row) {
-echo $row['country_code'].'&nbsp'. $row['count'] ."<br>";
+//5.répartition par etat +nombre d'enregistrement par état croissant
+// $sql = "SELECT country_code, COUNT(*) as count FROM `table 1` GROUP BY country_code ORDER BY count";
+// foreach ($conn -> query($sql) as $row) {
+// echo $row['country_code'].'&nbsp'. $row['count'] ."<br>";
+// }
 
+//6.Insérer un utilisateur, lui mettre à jour son adresse mail, puis supprimer l’utilisateur
 
-}
+//7. Afficher  le nombre de femmes et d'hommes et leurs  moyennes d’âge
 
 
      ?>
