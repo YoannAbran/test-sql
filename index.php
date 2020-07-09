9452b85acd30c526c32f4d9f8be4be1b5e94cd0e
Yoann Abran <y.abran@codeur.online>
2020-07-09 11:19:52 +0200

    exercice sql

--- index.php
+++ index.php
@@ -54,9 +54,59 @@
 
 //6.Insérer un utilisateur, lui mettre à jour son adresse mail, puis supprimer l’utilisateur
 
+//////insere utilisateur
+// try{
+//   $sql = "INSERT INTO `table 1` (first_name, last_name, email, gender, ip_address, birth_date, avatar_url, country_code) VALUES ('Bob','Lennon', 'bob@lennon.com', 'male', '127.0.0.1', '03/05/1990', 'http://boblennon.net/bob.jpg', 'FR')";
+//   // use exec() because no results are returned
+//   $conn->exec($sql);
+//   echo "New record created successfully";
+// }
+// catch(PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
+// $conn = null;
+
+/////update email
+//
+//   $sql = "UPDATE `table 1` SET email='Lennon@Bob.com' WHERE last_name = 'Lennon'";
+//
+//   // Prepare statement
+//   $stmt = $conn->prepare($sql);
+//
+//   // execute the query
+//   $stmt->execute();
+//
+//   // echo a message to say the UPDATE succeeded
+//   echo $stmt->rowCount() . " records UPDATED successfully";
+//
+// $conn = null;
+
+/////supprimer
+// $sql = "DELETE FROM `table 1` WHERE last_name='Lennon'";
+// $conn->exec($sql);
+//   echo "Record deleted successfully";
+//
+//   $conn = null;
 //7. Afficher  le nombre de femmes et d'hommes et leurs  moyennes d’âge
 
+///remplace les / par des -
+//UPDATE `table 1`
+//SET birth_date = REPLACE(birth_date, '/', '-')
 
+///modifie le dateformat pour convertir birth_date varchar->DATE
+//UPDATE `table 1` SET `birth_date`= DATE_FORMAT(str_to_date( birth_date, '%d-%m-%Y'),'%Y-%m-%d');
+
+///convertit en DateTime
+//SELECT CONVERT(birth_date, DATE)
+// FROM `table 1`
+
+$avg = "SELECT gender, YEAR(NOW())-AVG(YEAR(birth_date)) as avg, COUNT(gender) as count FROM `table 1` GROUP BY gender";
+foreach ($conn -> query($avg) as $row) {
+echo  $row['count'].'&nbsp'.$row['gender'] .'&nbsp'. $row['avg'] ."<br>";
+}
+
+// SELECT birth_date, REPLACE(birth_date, '/', '-')
+// FROM `table 1`
      ?>
   </body>
 </html>
